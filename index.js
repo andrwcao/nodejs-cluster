706bd58b6e65af85e87efda84725daf6d2d8cd09
andrwcao <80874259+andrwcao@users.noreply.github.com>
2022-12-25 22:43:05 +1100

    Added clustering

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+const cluster = require('cluster');
 const express = require('express');
 const app = express();
 
-function doWork(duration) {
-    const start = Date.now();
-    while (Date.now() - start < duration) {}
+if (cluster.isMaster) {
+
+    cluster.fork();
+} else {
+    function doWork(duration) {
+        const start = Date.now();
+        while (Date.now() - start < duration) {}
+    }
+
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send('Hi there');
+    });
+
+    app.get('/fast', (req, res) => {
+        res.send('This was fast');
+    });
+
+    app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
-});
-
-app.listen(3000);
